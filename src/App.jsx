153594b00deeb3e3cffaaf272595f9fb4ea56fd8
gEdisonCode <g.edison.code@gmail.com>
2025-03-11 09:09:46 -0300

    implementando api de menu de comida

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ function App() {
     <>
       <NavBar />
       <Header />
-      <Plato />
       <Menu />
+      <Plato />
     </>
   );
 }
--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -2,35 +2,66 @@ import React, { useState, useEffect } from "react";
 import style from "./menu.module.css";
 
 function Menu() {
-  const [menu, setMenu] = useState(false);
+  const [comida, setComida] = useState([]);
+  const [pedido, setPedido] = useState("Pasta");
+  const [renderizar, setRenderizar] = useState(0);
+  let url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${pedido}`;
 
-  function mostrar() {
-    setMenu(!menu);
+  function desayuno() {
+    setPedido("breakfast");
   }
+
+  function postre() {
+    setPedido("dessert");
+  }
+
+  function principal() {
+    setPedido("Pasta");
+  }
+
+  useEffect(() => {
+    async function fetchData() {
+      const respuesta = await fetch(url);
+      const data = await respuesta.json();
+      if (data.meals) {
+        setComida(data.meals);
+      }
+    }
+    fetchData();
+  }, [pedido]);
+
+  if (!comida) return <p>Cargando...</p>;
 
   return (
     <>
       <section className={style.contenedor}>
-        <button className={style.botón} onClick={mostrar}>
-          <img
-            src="./icons/menu.svg"
-            alt="botón-Imagen"
-            className={style.menu}
-          />
-        </button>
-        {menu === true && (
-          <div className={style.lista}>
-            <button className={style.opción}>desayuno</button>
-            <button className={style.opción}>almuerzo</button>
-            <button className={style.opción}>cena</button>
-          </div>
-        )}
-
-        <div className={style.tarjeta}>
-            <div className={style.imagen}></div>
-            <h2 className={style.titulo}></h2>
+        <h1 className={style.carta}>carta</h1>
+        <div className={style.lista}>
+          <button className={style.opción} onClick={desayuno}>
+            Desayuno
+          </button>
+          <button className={style.opción} onClick={postre}>
+            Postre
+          </button>
+          <button className={style.opción} onClick={principal}>
+            Principal
+          </button>
         </div>
 
+        <div className={style.contenedorTarjeta}>
+          {comida.slice(0, 8).map((item, index) => (
+            <div key={index} className={style.tarjeta}>
+              <div className={style.imagen}>
+                <img
+                  src={item.strMealThumb}
+                  alt="comida"
+                  className={style.imagen}
+                />
+              </div>
+              <h2 className={style.titulo}>{item.strMeal}</h2>
+            </div>
+          ))}
+        </div>
       </section>
     </>
   );
