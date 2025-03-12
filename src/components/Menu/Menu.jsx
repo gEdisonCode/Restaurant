b7328cb435dac4cda21a94657105040b5985f8fa
gEdisonCode <g.edison.code@gmail.com>
2025-03-11 23:41:24 -0300

    Implementando tarjetas de la api

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -4,9 +4,14 @@ import style from "./menu.module.css";
 function Menu() {
   const [comida, setComida] = useState([]);
   const [pedido, setPedido] = useState("Pasta");
-  const [renderizar, setRenderizar] = useState(0);
+  const [selección, setSelección] = useState(false);
+  const [compra, setCompra] = useState([]);
+
   let url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${pedido}`;
 
+  function mostrar() {
+    setSelección(!selección);
+  }
   function desayuno() {
     setPedido("breakfast");
   }
@@ -18,6 +23,16 @@ function Menu() {
   function principal() {
     setPedido("Pasta");
   }
+
+  function añadir(item) {
+    setCompra([...compra, item]);
+  }
+
+  function eliminar(quitar) {
+  setCompra(compra.filter((item) => item !== quitar))
+  }
+
+  console.log(compra);
 
   useEffect(() => {
     async function fetchData() {
@@ -35,6 +50,8 @@ function Menu() {
   return (
     <>
       <section className={style.contenedor}>
+
+
         <h1 className={style.carta}>carta</h1>
         <div className={style.lista}>
           <button className={style.opción} onClick={desayuno}>
@@ -49,8 +66,11 @@ function Menu() {
         </div>
 
         <div className={style.contenedorTarjeta}>
-          {comida.slice(0, 8).map((item, index) => (
-            <div key={index} className={style.tarjeta}>
+          {comida.slice(0, 4).map((item, index) => (
+            <div
+              key={index}
+              className={style.tarjeta}
+              onClick={() => añadir(item)}>
               <div className={style.imagen}>
                 <img
                   src={item.strMealThumb}
@@ -61,7 +81,24 @@ function Menu() {
               <h2 className={style.titulo}>{item.strMeal}</h2>
             </div>
           ))}
+
+          
         </div>
+
+        <button type="button" className={style.carrito} onClick={mostrar}>
+          <img
+            src="./icons/carrito.svg"
+            alt="carrito"
+            className={style.carritoImagen}
+          /> <span className={style.notificación}>{compra.length}</span> 
+        </button>
+        <div className={style.contenedorLista}>{selección === true &&
+          compra.map((item) => (
+            <ul className={style.seleccionado} key={item.idMeal}>
+              <li onClick={() => eliminar(item)}>{item.strMeal}</li> 
+            </ul>
+          ))}</div>
+
       </section>
     </>
   );
