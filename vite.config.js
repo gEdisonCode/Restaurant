959391d07f5c394fd8c3912eaf7d66e91a1569e5
gEdisonCode <g.edison.code@gmail.com>
2025-04-07 23:03:45 -0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://gEdisonCode.github.io/Restaurant/",
+  base: "https://gedisoncode.github.io/Restaurant/",
 })
